00575c1dd789abe6bf7d3753406668b598f0a066
twerkinghamtori <zxc2289@naver.com>
2023-08-18 01:43:18 +0900

    0818 주석 추가

--- project/backend/application.py
+++ project/backend/application.py
@@ -2,30 +2,34 @@ from transformers import BertTokenizer, BertModel
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
-import openai  # openai 모듈을 import
+import openai  # openai 모듈 import
 import sys
+
 from flask import Flask, render_template, request
 from project.model import main
 
 application = Flask(__name__)
 
-openai.api_key = "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"
+openai.api_key = "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"
 
+#메인 페이지
 @application.route("/")
 def main_page():
     return render_template("main_page.html")
 
+#정보 입력 페이지
 @application.route("/info")
 def info():
     return render_template("info.html")
 
+#결과 페이지
 @application.route("/report", methods=['POST', 'GET'])
 def report(): 
     if request.method == 'POST':
         data = request.form
         name = data.get('name') #이름
         age = data.get('age') #나이
-        family = data.getlist('family') #가족구성원
+        family = data.getlist('family') #가족구성원(부양가족)
         family_members = ', '.join(family)
         job = data.get('job') #종사직
         loan = data.get('loan') #기존대출여부
@@ -42,8 +46,6 @@ def report():
             query_sentence2 += f" 상품명은 {loan_name}, 상환 잔여 기간은 {loan_time}개월, 현재 이자율은 {loan_rate}%인"
          
         asset = data.get('asset') #자산규모
-        certificate = data.get('certificate') #공인인증서 사용여부
-        certificate_status = "사용하지않고" if certificate == "n" else "사용하고"
         others = data.get('others') #기타 특이사항
         
         query_sentence3 = f"자산규모는 {asset}원이고, 기타 특이사항은 {others}인 {job}인" 
@@ -55,8 +57,8 @@ def report():
         sys.stdout = sys.__stdout__  # stdout을 원래의 값으로 설정
 
         print("query_sentence1:", query_sentence1)  # 터미널에 출력
-        print("query_sentence2:", query_sentence2)  # 터미널에 출력
-        print("query_sentence3:", query_sentence3)  # 터미널에 출력
+        print("query_sentence2:", query_sentence2)  
+        print("query_sentence3:", query_sentence3)  
         
 
         sys.stdout = original_stdout  # stdout을 원래 값으로 복원
